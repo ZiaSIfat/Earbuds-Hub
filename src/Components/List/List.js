67d3eb03618ceb60b0507cce459957edbe2c5d5a
ZiaSifat <ziasifat150@gmail.com>
2022-03-28 14:25:33 +0600

    readme file updated

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './List.css'
 const List = (props) => {
     const { list } = props;
+    // console.log(list.name)
     return (
         <div className='listed-items'>
             <h2>Selected Items</h2>
             {
-                list.map(item => <h4 key={item.id}>Name: {item.name}</h4>)
+                list.map(item =>
+                    <h4 key={item.id}>Name: {item.name}</h4>)
 
             }
             <button onClick={() => props.randomItems(props.product)} className='list-btn'>Choose One</button>
